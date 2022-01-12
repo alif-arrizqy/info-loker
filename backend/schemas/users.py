418ce53af271d39d935e9a14f7ee049d3befa59e
alif-arrizqy <alif.arrizqy20@gmail.com>
2022-01-12 15:49:12 +0700

    create User endpoint

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -4,3 +4,11 @@ class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+class ShowUser(BaseModel):
+    username: str
+    email: EmailStr
+    is_active: bool
+
+    class Config():
+        orm_mode = True
