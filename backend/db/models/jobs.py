5e4e1b7d2f41a310ff7a38b47cbc49f7a0886a9b
alif-arrizqy <alif.arrizqy20@gmail.com>
2022-01-12 15:31:45 +0700

    change column tables

--- backend/db/models/jobs.py
+++ backend/db/models/jobs.py
@@ -10,7 +10,7 @@ class Job(Base):
     company = Column(String, nullable=False)
     company_url = Column(String)
     location = Column(String, nullable=False)
-    description = Column(String)
+    description = Column(String, nullable=False)
     date_posted = Column(Date)
     is_active = Column(Boolean(), default=True)
     owner_id = Column(Integer, ForeignKey("user.id"))
--- backend/db/models/users.py
+++ backend/db/models/users.py
@@ -7,9 +7,8 @@ from db.base_class import Base
 class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, nullable=False)
-    email = Column(String, unique=True, nullable=False, index=True)
+    email = Column(String, nullable=False, unique=True, index=True)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
-    owner_id = Column(Integer, ForeignKey("user.id"))
-    owner = relationship("Job", back_populates="owner")
+    jobs = relationship("Job", back_populates="owner")
