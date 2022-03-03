620884216d3500a381cbf9f3e17037cae6722e2f
alif-arrizqy <alif.arrizqy20@gmail.com>
2022-03-03 10:50:58 +0700

    postgres conf

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,13 +1,19 @@
 from typing import Generator
-from sqlalchemy import create_engine, engine
+
+from core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from core.config import settings
 
-SQL_ALCHEMY_DATABASE_URL = "sqlite:///./mydatabase.db"
-engine = create_engine(
-    SQL_ALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+
+
+if settings.USE_SQLITE_DB == "True":
+	SQLALCHEMY_DATABASE_URL = "sqlite:///./mydatabase.db"
+	engine = create_engine(
+		SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+	)
+else:
+	SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+	engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -16,6 +22,5 @@ def get_db() -> Generator:
     try:
         db = SessionLocal()
         yield db
-    except:
-        db = SessionLocal()
+    finally:
         db.close()
